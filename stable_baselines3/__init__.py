c43f9afb93e1e32f2b3a600160f6447ad5251289
JaniceZhou <janicezhou828@gmail.com>
2021-02-03 16:19:06 -0500

    removed SAC

--- stable_baselines3/__init__.py
+++ stable_baselines3/__init__.py
@@ -2,7 +2,6 @@ import os
 
 from stable_baselines3.a2c import A2C
 from stable_baselines3.ppo import PPO
-from stable_baselines3.sac import SAC
 
 # Read version from file
 version_file = os.path.join(os.path.dirname(__file__), "version.txt")
