dc488f6e9d1d17afe9690bdf16d5509f9f816b71
JaniceZhou <janicezhou828@gmail.com>
2021-02-07 17:26:30 -0500

    test modification on rollout collection for playing against oppenent (17): created new volley_env in rollout at the beginning of the session, investigatig self vs opponent observations

--- stable_baselines3/common/on_policy_algorithm.py
+++ stable_baselines3/common/on_policy_algorithm.py
@@ -154,7 +154,7 @@ class OnPolicyAlgorithm(BaseAlgorithm):
             self.policy.reset_noise(env.num_envs)
 
         callback.on_rollout_start()
-
+        volley_env = gym.make("SlimeVolley-v0")
         while n_steps < n_rollout_steps:
             if self.use_sde and self.sde_sample_freq > 0 and n_steps % self.sde_sample_freq == 0:
                 # Sample a new noise matrix
@@ -188,7 +188,7 @@ class OnPolicyAlgorithm(BaseAlgorithm):
             if isinstance(self.action_space, gym.spaces.Box):
                 clipped_actions_op = np.clip(actions_op, self.action_space.low, self.action_space.high)
 
-            volley_env = gym.make("SlimeVolley-v0")
+            
             new_obs, rewards, dones, info = volley_env.step(clipped_actions[0],clipped_actions_op[0])
             ####print("line 192: new_obs, rewards, dones, infos", new_obs, rewards, dones, info)
             ################new_obs, rewards, dones, infos = env.step(clipped_actions)
