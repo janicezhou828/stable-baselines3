fefc2a302239c0e163af11fa3bb8773afa300d3d
JaniceZhou <janicezhou828@gmail.com>
2021-02-07 16:51:47 -0500

    test modification on rollout collection for playing against oppenent (14): created new volley_env in rollout, fixed otherObs

--- stable_baselines3/common/on_policy_algorithm.py
+++ stable_baselines3/common/on_policy_algorithm.py
@@ -189,7 +189,7 @@ class OnPolicyAlgorithm(BaseAlgorithm):
                 clipped_actions_op = np.clip(actions_op, self.action_space.low, self.action_space.high)
 
             volley_env = gym.make("SlimeVolley-v0")
-            new_obs, rewards, dones, infos = volley_env.step(clipped_actions[0],clipped_actions_op[0])
+            new_obs, rewards, dones, info = volley_env.step(clipped_actions[0],clipped_actions_op[0])
             print("line 192: new_obs, rewards, dones, infos", new_obs, rewards, dones, info)
             #new_obs, rewards, dones, infos = env.step(clipped_actions)
             #new_obs, rewards, dones, infos = volley_env.step(clipped_actions[0])
